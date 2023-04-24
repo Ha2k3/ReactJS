d802fc98a8dfe8b528e0cf2975a3032da265ab03
Thuy Ha <116275455+Ha2k3@users.noreply.github.com>
2023-04-24 18:05:58 +0700

    updatehomework

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import Footer from './Components/Footer/Footer';
 import Slider from './Components/Content/Slider/Slider';
 import LeftContent from './Components/Content/LeftContent/LeftContent';
 import RightContent from './Components/Content/RightContent/RightContent';
+import Member from './Components/Member/Member';
+import State1 from './Components/State1/State1';
+import Vd1 from './Components/Hooks/Vd1';
+import Vd2 from './Components/Hooks/Vd2';
+import Vd3 from './Components/Hooks/Vd3';
+import Car from './Components/Hooks/Car';
+import Login from './Components/Hooks/Login';
+import Ketquahoctap from './Components/BaiKQHT/Ketquahoctap';
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Slider></Slider>
-      <div class="container">
+      {/* <Header></Header>
+      <Slider></Slider> */}
+      {/* <div class="container">
         <div className='row'>
           <LeftContent></LeftContent>
           <RightContent></RightContent>
         </div>
       </div>
-      <Footer></Footer>
+      <Footer></Footer> */}
+      {/* <Member></Member> */}
+      {/* <State1></State1> */}
+      {/* <Vd1></Vd1> */}
+      {/* <Vd2></Vd2> */}
+      {/* <Vd3></Vd3> */}
+      {/* <Car></Car> */}
+      {/* <Login></Login> */}
+      {/* <DemoForm1></DemoForm1> */}
+      <Ketquahoctap></Ketquahoctap>
     </div>
   );
 }
